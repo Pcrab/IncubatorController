beb6813f5d3253b596806ad1184fc81718fc0c8d
Pcrab <pcrab@pcrab.xyz>
2022-02-12 20:22:05 +0800

    add logout support

--- src/components/Incubators.vue
+++ src/components/Incubators.vue
@@ -1,12 +1,51 @@
 <template>
   <div class="incubators">
     Incubators
+    <div @click="refresh">获取</div>
+    <div @click="logout">登出</div>
+    <div>{{ result }}</div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Incubators',
+  data() {
+    return {
+      result: "",
+    }
+  },
+  methods: {
+    refresh() {
+      fetch(this.$uri + "/user/incubator", {
+        method: "GET", mode: "cors", credentials: "include"
+      }).then(response => {
+        if (response.ok) {
+          response.json().then(json => {
+            console.log(json.incubators)
+          })
+        } else {
+          response.text().then(text => {
+            console.log(text)
+          })
+        }
+          })
+    },
+    logout() {
+      fetch(this.$uri + "/user/logout", {
+        method: "GET", mode: "cors", credentials: "include"
+      }).then(response => {
+        if (response.ok) {
+          console.log("Logout succeeded")
+          location.reload()
+        } else {
+          response.text().then(text => {
+            this.result = text
+          })
+        }
+      })
+    },
+  }
 }
 </script>
 
